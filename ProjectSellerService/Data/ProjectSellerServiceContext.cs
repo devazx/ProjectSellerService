894e5e94f5d7355cc2775210628eeb7137c6c03b
Daniel Vaz <danielvaz.tzk@hotmail.com>
2024-03-20 20:14:11 -0300

    CRUD Scaffolding

---
+++ ProjectSellerService/Data/ProjectSellerServiceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectSellerService.Models
+{
+    public class ProjectSellerServiceContext : DbContext
+    {
+        public ProjectSellerServiceContext (DbContextOptions<ProjectSellerServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectSellerService.Models.Department> Department { get; set; } = default!;
+    }
+}
