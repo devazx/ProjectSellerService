47b7d96269b2ea04cb10219fd0085e0f447f8aea
Daniel Vaz <danielvaz.tzk@hotmail.com>
2024-03-21 23:58:47 -0300

    SellerController

---
+++ ProjectSellerService/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectSellerService.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
