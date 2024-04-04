4fbafc7e37c1d6b3a478bb0cc34e5e917bcc5d2a
Daniel Vaz <danielvaz.tzk@hotmail.com>
2024-04-03 23:29:53 -0300

    Delete Seller - on error

--- ProjectSellerService/Controllers/SellersController.cs
+++ ProjectSellerService/Controllers/SellersController.cs
@@ -37,5 +37,20 @@ namespace ProjectSellerService.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+        
+        public IActionResult Delete(int? id) 
+        { 
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
